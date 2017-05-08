5cc4fd1a26b997ba52fcaa18681f5ef0f7e54578
Gabriel Sánchez <gabrielsanchez.frontend@gmail.com>
2017-05-08 15:01:07 -0500

    Primer commit

---
+++ js/base-datos.js
@@ -0,0 +1,245 @@
+var listMenu = [
+    {
+        name: 'Home',
+        href: 'nav-home',
+        ico: 'fa-home'
+    },
+    {
+        name: 'Features',
+        href: 'nav-features',
+        ico: 'fa-plus-square'
+    },
+    {
+        name: 'Gallery',
+        href: 'nav-gallery',
+        ico: 'fa-camera'
+    },
+    {
+        name: 'Video',
+        href: 'nav-video',
+        ico: 'fa-youtube-play'
+    },
+    {
+        name: 'Prices',
+        href: 'nav-prices',
+        ico: 'fa-usd'
+    },
+    {
+        name: 'Testimonials',
+        href: 'nav-testimonials',
+        ico: 'fa-comments'
+    },
+    {
+        name: 'Download',
+        href: 'nav-download',
+        ico: 'fa-download'
+    },
+    {
+        name: 'Contact',
+        href: 'nav-contact',
+        ico: 'fa-user'
+    }
+];
+
+var arrays = {
+        listMenu: [
+        {
+            name: 'Home',
+            href: 'nav-home',
+            ico: 'fa-home'
+        },
+        {
+            name: 'Features',
+            href: 'nav-features',
+            ico: 'fa-plus-square'
+        },
+        {
+            name: 'Gallery',
+            href: 'nav-gallery',
+            ico: 'fa-camera'
+        },
+        {
+            name: 'Video',
+            href: 'nav-video',
+            ico: 'fa-youtube-play'
+        },
+        {
+            name: 'Prices',
+            href: 'nav-prices',
+            ico: 'fa-usd'
+        },
+        {
+            name: 'Testimonials',
+            href: 'nav-testimonials',
+            ico: 'fa-comments'
+        },
+        {
+            name: 'Download',
+            href: 'nav-download',
+            ico: 'fa-download'
+        },
+        {
+            name: 'Contact',
+            href: 'nav-contact',
+            ico: 'fa-user'
+        }
+    ],
+    banner: [
+        {
+            idSlide: 1,
+            nSlide: 'slide1',
+            appName: 'Telegram',
+            appIco: 'fa fa-telegram',
+            subtitle: 'Telegrams is a messaging application focused on speed and security.',
+            description: 'Pure and instant messaging. Simple, fast, secure and synchronized across all your devices. More than 100 million active users in two and a half years.'
+        },
+        {
+            idSlide: 2,
+            nSlide: 'slide2',
+            appName: 'Instagram',
+            appIco: 'fa fa-instagram',
+            subtitle: 'An excellent way to share your world. Fast, free and fun.',
+            description: 'Instagram is a simple way to capture and share the best moments in the world. Follow your friends and family to know what they are doing and discover accounts from around the world that share things you love.'
+        },
+        {
+            idSlide: 3,
+            nSlide: 'slide3',
+            appName: 'Spotify',
+            appIco: 'fa fa-spotify',
+            subtitle: 'Spotify gives you instant access to millions of songs on your Android or Iphone.',
+            description: 'Now Spotify is free for mobile and tablet. Listen to the perfect music wherever you are. You can listen to artists and albums or create your own playlist with your favorite songs.'
+        }
+    ],
+    summarise: [
+        {
+            ico: 'fa fa-file-text-o',
+            name: 'Atractive Layout',
+            description: 'Design is paramount because it\'s exactly the design that makes a website appealing, even one that does not contain eye-catching graphics.'
+        },
+        {
+            ico: 'fa fa-mobile fa-lg',
+            name: 'Fresh design',
+            description: 'Simplicity is easier to understand. By doing things simple, your audience is not overwhelmed and focuses on the message you want to convey.'
+        },
+        {
+            ico: 'fa fa-comments fa-lg',
+            name: 'Multipurpose',
+            description: 'Perform various tasks through your website, share ideas, sell products, communicate with your customers and position your brand the way you want.'
+        },
+        {
+            ico: 'fa fa-desktop',
+            name: 'Easy to customize',
+            description: 'Customize your digital space to your liking, so you can find what you are looking for in the shortest possible time. Start now!'
+        }
+    ],
+    gallery: [
+        {
+            idPop: 1,
+            img: 'images/gallery1.jpg',
+            alt: 'ScreenShot#1',
+            class: 'screenShot-1',
+            name: 'ScreenShot #1'
+        },
+        {
+            idPop: 2,
+            img: 'images/gallery2.jpg',
+            alt: 'ScreenShot#2',
+            class: 'screenShot-2',
+            name: 'ScreenShot #2'
+        },
+        {
+            idPop: 3,
+            img: 'images/gallery3.jpg',
+            alt: 'ScreenShot#3',
+            class: 'screenShot-3',
+            name: 'ScreenShot #3'
+        },
+        {
+            idPop: 4,
+            img: 'images/gallery4.jpg',
+            alt: 'ScreenShot#4',
+            class: 'screenShot-4',
+            name: 'ScreenShot #4'
+        }
+    ],
+    popImg: [
+        {
+            img: 'images/pop1.jpg',
+            alt: 'PopImg#1',
+            class: 'popImg-1'
+        },
+        {
+            img: 'images/pop2.jpg',
+            alt: 'PopImg#2',
+            class: 'popImg-2'
+        },
+        {
+            img: 'images/pop3.jpg',
+            alt: 'PopImg#3',
+            class: 'popImg-3'
+        },
+        {
+            img: 'images/pop4.jpg',
+            alt: 'PopImg#4',
+            class: 'popImg-4'
+        }
+    ],
+    prices: [
+        {
+            name: 'Basic Package',
+            class: 'price-basic',
+            price: '20$',
+            description: {
+                item1: 'Donec vel accumsan quam.',
+                item2: 'Phasellus at massa vel.',
+                item3: 'Quisque eu elit vitae.',
+                item4: '---'
+            },
+            classButton: 'purchase-basic'
+        },
+        {
+            name: 'Profesional Package',
+            class: 'price-profesional',
+            price: '20$',
+            description: {
+                item1: 'Etiam ut scelerisque sem.',
+                item2: 'Vestibulum sit amet ante.',
+                item3: 'Aliquam pretium, erat vitae.',
+                item4: 'Nulla facilisi. Etiam ut.'
+            },
+            classButton: 'purchase-profesional'
+        },
+        {
+            name: 'Advanced Package',
+            class: 'price-advanced',
+            price: '20$',
+            description: {
+                item1: 'Mauris tempus nisl feugiat.',
+                item2: 'Sed id scelerisque turpis.',
+                item3: 'Cras in eros sed.',
+                item4: '---'
+            },
+            classButton: 'purchase-advanced'
+        }
+    ],
+    testimonials: [
+        {
+            img: 'images/test1.png',
+            altImg: 'Testimonial#1',
+            paragraph: 'Cras aliquam egestas posuere. Mauris maximus posuere augue vitae aliquam. Fusce egestas sagittis velit, a pharetra magna eleifend eu. Cras id aliquam nisi. Donec id fermentum ex. Nulla facilisi. Integer dui mi, tincidunt ac nulla at, finibus fringilla est.',
+            class: 'test-1'
+        },
+        {
+            img: 'images/test2.png',
+            altImg: 'Testimonial#2',
+            paragraph: 'Integer auctor erat eget ipsum tempus laoreet. Maecenas ornare, est a congue sodales, lorem nisi mattis lacus, sit amet pellentesque purus nisl eget magna. Sed tristique purus nisi, id cursus nunc pellentesque ut. In eleifend scelerisque mattis.',
+            class: 'test-2'
+        },
+        {
+            img: 'images/test3.png',
+            altImg: 'Testimonial#3',
+            paragraph: 'Nunc odio nibh, eleifend sed sapien id, vulputate dapibus nisi. Etiam et nisi lectus. Maecenas consectetur ultricies semper. Nam ut massa consequat, pharetra odio sed, ultricies justo. Suspendisse malesuada velit at pellentesque aliquet.',
+            class: 'test-3'
+        }
+    ]
+};
